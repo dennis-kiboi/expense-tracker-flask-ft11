bdd541556ed6a4425042617729c8f429a0eb7920
Dennis Kiboi <dennis.kiboi@moringaschool.com>
2025-01-20 10:05:34 +0300

    Refactored CRUD view functions for user model

--- app.py
+++ app.py
@@ -17,55 +17,44 @@ def home():
     return '<h1>Welcome to Expense Tracker App</h1>'
 
 # CRUD operations for User model
-@app.route('/users')
-def get_all_users():
-    return make_response([user.to_dict() for user in User.query.all()], 200)
-
-@app.route('/users/<int:id>', methods=['GET'])
-def get_user(id):
-    user = User.query.get(id)
-
-    if user:
-        return make_response(user.to_dict(), 200)
-    
-    return make_response({'error': 'User not found'}, 404)
-
-@app.route('/users/<int:id>', methods=['DELETE'])
-def delete_user(id):
-    user = User.query.get(id)
-
-    if user:
-        db.session.delete(user)
-        db.session.commit()
-
-        return make_response({'message': 'User deleted successfully'}, 200)
-    
-    return make_response({'error': 'User not found'}, 404)
-
-@app.route('/users', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    user = User(name=data['name'], email=data['email'])
-    db.session.add(user)
-    db.session.commit()
-    return make_response(user.to_dict(), 201)
-
-@app.route('/users/<int:id>', methods=['PATCH'])
-def update_user(id):
-    user = User.query.get(id)
-
-    if user:
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    if request.method == 'GET':
+        return make_response([user.to_dict() for user in User.query.all()], 200)
+    elif request.method == 'POST':
         data = request.get_json()
-
-        if 'name' in data:
-            user.name = data['name']
-        if 'email' in data:
-            user.email = data['email']
-
+        user = User(name=data['name'], email=data['email'])
+        db.session.add(user)
         db.session.commit()
-        return make_response(user.to_dict(), 200)
-    
-    return make_response({'error': 'User not found'}, 404)
+        return make_response(user.to_dict(), 201)
+
+@app.route('/users/<int:id>', methods=['GET', 'PATCH', 'DELETE'])
+def user_by_id(id):
+    user = User.query.get(id)
+
+    if user == None:
+        return make_response({'error': 'User not found'}, 404)
+
+    else:
+        if request.method == 'GET':
+            return make_response(user.to_dict(), 200)
+
+        elif request.method == 'PATCH':
+            data = request.get_json()
+
+            if 'name' in data:
+                user.name = data['name']
+            if 'email' in data:
+                user.email = data['email']
+
+            db.session.commit()
+            return make_response(user.to_dict(), 200)
+
+        elif request.method == 'DELETE':
+            db.session.delete(user)
+            db.session.commit()
+            return make_response({'message': 'User deleted successfully'}, 200)
+
 
 @app.route('/transactions/<int:id>')
 def get_transaction(id):
